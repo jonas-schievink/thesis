2043922131a0c7992656349e417d607ef0d8e5fc
Jonas Schievink <jonasschievink@gmail.com>
2018-03-22 16:58:26 +0100

    more useful PID config logging

--- src/PIDController.cpp
+++ src/PIDController.cpp
@@ -7,7 +7,7 @@
 PIDController::PIDController(float kp, float ki, float kd, float integralLimit) :
 m_setpoint(0.0f), m_effort(0.0f), m_integral(0.0f), m_lastError(0.0f),
 m_kp(kp), m_ki(ki), m_kd(kd), m_integralLimit(integralLimit) {
-    ROS_INFO("PIDController ctor: kp=%-5.3f ki=%-5.3f kd=%-5.3f ilimit=%-5.3f", kp, ki, kd, integralLimit);
+    ROS_INFO("PIDController ctor: kp=%-.5f ki=%-.5f kd=%-.5f ilimit=%-.5f", kp, ki, kd, integralLimit);
 }
 
 PIDController::PIDController() : PIDController(0, 0, 0, 0) {}
