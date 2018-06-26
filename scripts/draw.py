010692c983cc87109c6f1f80ca8fbcab48b71014
Jonas Schievink <jonasschievink@gmail.com>
2018-06-26 18:33:12 +0200

    calc min/max axis values

--- scripts/draw.py
+++ scripts/draw.py
@@ -2,23 +2,40 @@
 
 import matplotlib.pyplot as plt
 
+minx = 0.0
+maxx = 0.0
+miny = 0.0
+maxy = 0.0
+
 fodom = open('plot-odom.csv', 'r')
 odomx = []
 odomy = []
 for line in fodom.readlines():
     xy = line.split(',')
-    odomx.append(xy[0])
-    odomy.append(xy[1])
+    x = float(xy[0])
+    y = float(xy[1])
+    minx = min(minx, x)
+    maxx = max(maxx, x)
+    miny = min(miny, y)
+    maxy = max(maxy, y)
+    odomx.append(x)
+    odomy.append(y)
 
 fcmdvel = open('plot-cmdvel.csv', 'r')
 cmdvelx = []
 cmdvely = []
 for line in fcmdvel.readlines():
     xy = line.split(',')
-    cmdvelx.append(xy[0])
-    cmdvely.append(xy[1])
+    x = float(xy[0])
+    y = float(xy[1])
+    minx = min(minx, x)
+    maxx = max(maxx, x)
+    miny = min(miny, y)
+    maxy = max(maxy, y)
+    cmdvelx.append(x)
+    cmdvely.append(y)
 
-#plt.axis(xmin=0, ymin=0)
+plt.axis([minx, maxx+0.01, miny, maxy+0.01])
 cmdvel = plt.plot(cmdvelx, cmdvely, color='blue')
 odom = plt.plot(odomx, odomy, color='red')
 plt.legend(['Zielgeschwindigkeit (/cmd_vel)', 'Wirkliche Geschwindigkeit (/odom)'])
